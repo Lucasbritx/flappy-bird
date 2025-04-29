54c5e03971423a28723b8df836fa502e7c5f523c
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-29 08:50:35 -0300

    adding gravity to flappyBird

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const sprites = new Image();
-sprites.src = './sprites.png';
+sprites.src = "./sprites.png";
 
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
 const background = {
   spriteX: 390,
@@ -13,23 +12,31 @@ const background = {
   x: 0,
   y: canvas.height - 204,
   draw() {
-    context.fillStyle = '#70c5ce';
-    context.fillRect(0,0, canvas.width, canvas.height)
+    context.fillStyle = "#70c5ce";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
     context.drawImage(
       sprites,
-      background.spriteX, background.spriteY,
-      background.width, background.height,
-      background.x, background.y,
-      background.width, background.height,
+      background.spriteX,
+      background.spriteY,
+      background.width,
+      background.height,
+      background.x,
+      background.y,
+      background.width,
+      background.height
     );
 
     context.drawImage(
       sprites,
-      background.spriteX, background.spriteY,
-      background.width, background.height,
-      (background.x + background.width), background.y,
-      background.width, background.height,
+      background.spriteX,
+      background.spriteY,
+      background.width,
+      background.height,
+      background.x + background.width,
+      background.y,
+      background.width,
+      background.height
     );
   },
 };
@@ -44,18 +51,26 @@ const ground = {
   draw() {
     context.drawImage(
       sprites,
-      ground.spriteX, ground.spriteY,
-      ground.width, ground.height,
-      ground.x, ground.y,
-      ground.width, ground.height,
+      ground.spriteX,
+      ground.spriteY,
+      ground.width,
+      ground.height,
+      ground.x,
+      ground.y,
+      ground.width,
+      ground.height
     );
 
     context.drawImage(
       sprites,
-      ground.spriteX, ground.spriteY,
-      ground.width, ground.height,
-      (ground.x + ground.width), ground.y,
-      ground.width, ground.height,
+      ground.spriteX,
+      ground.spriteY,
+      ground.width,
+      ground.height,
+      ground.x + ground.width,
+      ground.y,
+      ground.width,
+      ground.height
     );
   },
 };
@@ -67,23 +82,32 @@ const flappyBird = {
   height: 24,
   x: 10,
   y: 50,
+  gravity: 0.25,
+  speed: 0,
+  update() {
+    flappyBird.speed = flappyBird.speed * flappyBird.gravity;
+    flappyBird.y = flappyBird.y + flappyBird.speed;
+  },
   draw() {
     context.drawImage(
       sprites,
-      flappyBird.spriteX, flappyBird.spriteY,
-      flappyBird.width, flappyBird.height,
-      flappyBird.x, flappyBird.y,
-      flappyBird.width, flappyBird.height,
+      flappyBird.spriteX,
+      flappyBird.spriteY,
+      flappyBird.width,
+      flappyBird.height,
+      flappyBird.x,
+      flappyBird.y,
+      flappyBird.width,
+      flappyBird.height
     );
-  }
-}
+  },
+};
 
 function loop() {
   background.draw();
   ground.draw();
+  flappyBird.update();
   flappyBird.draw();
-
-  flappyBird.y = flappyBird.y + 1;
 
   requestAnimationFrame(loop);
 }
